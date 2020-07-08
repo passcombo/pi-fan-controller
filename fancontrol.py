1f52096ba31c97c55aa1ef919a87a723d5139fd0
passcombo <35246839+passcombo@users.noreply.github.com>
2020-07-08 19:12:32 +0200

    Added features
    1. Start log file, 
    2. loop logs, 
    3. start fan run for 15 seconds to be sure it is working, 
    4. different method of temperature measuring (more general for other rpi OS like Ubuntu)
    5. added chmod to be able to read and write gpio on Ubuntu

--- fancontrol.py
+++ fancontrol.py
@@ -1,55 +1,85 @@
 #!/usr/bin/env python3
-
+import datetime
 import subprocess
 import time
 
 from gpiozero import OutputDevice
 
 
-ON_THRESHOLD = 65  # (degrees Celsius) Fan kicks on at this temperature.
-OFF_THRESHOLD = 55  # (degress Celsius) Fan shuts off at this temperature.
-SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
+ON_THRESHOLD = 61  # (degrees Celsius) Fan kicks on at this temperature.
+OFF_THRESHOLD = 48  # (degress Celsius) Fan shuts off at this temperature.
+SLEEP_INTERVAL = 10  # (seconds) How often we check the core temperature.
 GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
+TEMPERATUR_SYS_PATH='/sys/class/thermal/thermal_zone0/temp'
+
+
+
+def log_time(file_path,mode='w'): #  'a+' for append
+
+        x=datetime.datetime.today()
+        time_format='%Y-%m-%d %H:%M:%S'
+
+        x1=x.strftime(time_format)
+
+        with open(file_path, mode) as f:
+
+                f.write(x1+'\n')
+                f.close()
+
+
+
 
 
 def get_temp():
-    """Get the core temperature.
+        """Get the core temperature.
+        Run a shell script to get the core temp and parse the output.
+        Raises:
+                RuntimeError: if response cannot be parsed.
+        Returns:
+                float: The core temperature in degrees Celsius.
+        """
+        output = subprocess.run(['cat',TEMPERATUR_SYS_PATH],capture_output=True)
+        temp_str = output.stdout.decode()
 
-    Run a shell script to get the core temp and parse the output.
+        try:
+                return float(temp_str)/1000
+        except (IndexError, ValueError):
+                raise RuntimeError('Could not parse temperature output.')
 
-    Raises:
-        RuntimeError: if response cannot be parsed.
-
-    Returns:
-        float: The core temperature in degrees Celsius.
-    """
-    output = subprocess.run(['vcgencmd', 'measure_temp'], capture_output=True)
-    temp_str = output.stdout.decode()
-    try:
-        return float(temp_str.split('=')[1].split('\'')[0])
-    except (IndexError, ValueError):
-        raise RuntimeError('Could not parse temperature output.')
-
+                
+                
+                
 
 if __name__ == '__main__':
-    # Validate the on and off thresholds
-    if OFF_THRESHOLD >= ON_THRESHOLD:
-        raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
 
-    fan = OutputDevice(GPIO_PIN)
+        tmp=subprocess.run(['sudo','chmod','o+rw','/dev/gpiomem'],capture_output=True)
 
-    while True:
-        temp = get_temp()
+        log_time('last_start.txt')
+        # Validate the on and off thresholds
+        if OFF_THRESHOLD >= ON_THRESHOLD:
+                raise RuntimeError('OFF_THRESHOLD must be less than ON_THRESHOLD')
 
-        # Start the fan if the temperature has reached the limit and the fan
-        # isn't already running.
-        # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
-        if temp > ON_THRESHOLD and not fan.value:
-            fan.on()
+        fan = OutputDevice(GPIO_PIN)
 
-        # Stop the fan if the fan is running and the temperature has dropped
-        # to 10 degrees below the limit.
-        elif fan.value and temp < OFF_THRESHOLD:
-            fan.off()
+        # test run to ensure it is running
+        fan.on()
+        time.sleep(15)
+        fan.off()
 
-        time.sleep(SLEEP_INTERVAL)
+
+        while True:
+                log_time('last_loop.txt','a+')
+
+                temp = get_temp()
+                # Start the fan if the temperature has reached the limit and the fan
+                # isn't already running.
+                # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
+                if temp > ON_THRESHOLD and not fan.value:
+                        fan.on()
+
+                # Stop the fan if the fan is running and the temperature has dropped
+                # to 10 degrees below the limit.
+                elif fan.value and temp < OFF_THRESHOLD:
+                        fan.off()
+
+                time.sleep(SLEEP_INTERVAL)
